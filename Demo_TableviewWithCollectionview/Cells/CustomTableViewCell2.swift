66b939f2dacbfd222c18f18aab6cba982aecaaed
Nick <nickusa1982@gmail.com>
2017-07-22 21:47:31 -0700

    OK

---
+++ Demo_TableviewWithCollectionview/Cells/CustomTableViewCell2.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell2.swift
+//  Demo_TableviewWithCollectionview
+//
+//  Created by Nick on 7/22/17.
+//  Copyright © 2017 NickOwn. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell2: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Demo_TableviewWithCollectionview/Cells/CustomTableViewCell3.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell3.swift
+//  Demo_TableviewWithCollectionview
+//
+//  Created by Nick on 7/22/17.
+//  Copyright © 2017 NickOwn. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell3: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
